d7f0eb1ba74b50431a2f39242ae3b1c3cd569347
tbrann99 <tbrann99@gmail.com>
2024-11-18 09:06:22 +0000

    multibar_plotter.py
    Updated hit definitions

--- Figure_4/multibar_plotter.py
+++ Figure_4/multibar_plotter.py
@@ -45,16 +45,56 @@ def send(name,P3,S3):
     elif(S3=="No" and P3!="No"):
         print("0,0")
 
+def push(TE,i):
+    ##Selected best TE
+    #print("")
+    ##Extract names
+    print(lol[i][3].split(" ")[0],end="")
+    if(len(lol[i][3].split(" "))>1):
+        print("_",end="")
+        print(lol[i][3].split(" ")[1],end=",")
+    else:
+        print(",", end="")
 
-
+    if(int(TE[1])>2000 and int(TE[3])>4):
+        print("100,0")
+    else:
+        print("0,100")
+"""     print(TE[0],end="\t")
+    print(TE[1],end="\t")
+    print(TE[2],end="\t")
+    print(TE[3],end="\t")
+    print(lol[i][0],end="\t")
+    print(lol[i][1],end="\t")
+    print(lol[i][2],end="\t")
+    print(lol[i][3]) """
 
 
 for i in range(0,count):
     name=lol[i][3]
-    P3=lol[i][4]
-    S3=lol[i][5]
+    P3=lol[i][4].split(",")
+    S3=lol[i][5].split(",")
 
-    S3_out=answer(S3.split(","))
-    P3_out=answer(P3.split(","))
+    #S3_out=answer(S3.split(","))
+    #P3_out=answer(P3.split(","))
+    if(len(P3)>2 or len(S3)>2):
+        if(int(P3[1])>1500 or int(S3[1])>1500):
+            if(len(P3)>2 and len(S3)>2):
+                if(int(P3[1])>int(S3[1])):
+                    push(P3,i)
+                elif(int(P3[1])==int(S3[1])):
+                    if(float(P3[0])>float(S3[0])):
+                        #print(P3)
+                        push(P3,i)
+                    elif(float(P3[0])==float(S3[0])):
+                        #print(P3,S3)
+                        push(P3,i)
+                    else:
+                        push(S3,i)
+                        #print(S3)
+                else:
+                    push(S3,i)  
+                    #print(P3,S3)
 
-    send(name,P3_out,S3_out)
+
+        #send(name,P3_out,S3_out)
